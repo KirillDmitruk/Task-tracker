291bb319d12f3a0a592ce0197c8231e99d9cb4eb
Kirill <kirill@kirill-VirtualBox>
2024-08-15 22:48:08 +0300

    add admin

--- task_tracker/admin.py
+++ task_tracker/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from task_tracker.models import Employee, Task
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'position', 'vacation_status',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'title', 'parent_task', 'employee', 'start_date', 'end_date', 'status', 'comments', 'owner', 'is_active',
+        'is_important',)
